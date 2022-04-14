22b9426a526093b5529d7ad375839ae2fae07da0
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-13 20:27:10 -0700

    create add note method for store class

--- db/store.js
+++ db/store.js
@@ -15,9 +15,27 @@ class Store {
     write(note) {
         return writeFileAsync('db/db.json', JSON.stringify(note));
     };
+
+    // add notes method
+    addNote(note) {
+        const { title, text } = note;
+
+        // check if note is missing title or text
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' must be filled in!");
+        }
+
+        // add unique id using uuid 
+        const newNote = { title, text, id: uuid() };
+
+        // get notes, add new note, write all notes again, return new note
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+    };
 // delete method
 // get notes method
-// add notes method
 };
 
 module.exports = new Store();
