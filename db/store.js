b8f4dd535cc04d76d2389013911d7eeb5731a063
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-13 20:09:47 -0700

    create read method for store class

--- db/store.js
+++ db/store.js
@@ -7,7 +7,15 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-
+    // read method
+    read() {
+        return readFileAsync('db/db.json', 'utf-8');
+    };
+// write method
+    // use writeFileAsync
+// delete method
+// get notes method
+// add notes method
 };
 
 module.exports = new Store();
