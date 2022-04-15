8e66620fe0a8d211d633e9a7139001e6e0892771
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-14 17:09:57 -0700

    add html routes

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,16 @@
+// dependencies
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+// store class
+const store = require('../../db/store');
+
+// sends notes.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// all other routes send index.html file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
